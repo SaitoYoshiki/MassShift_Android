0900439213bda06a3dde14cac18dd0ca50f70065
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-14 01:01:19 +0900

    エディターに関する便利な機能をまとめた、EditorUtilityクラスを作成

---
+++ MassShift/Assets/Scripts/Saito/Editor/EditorUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorUtility {
+
+	public static bool IsPrefab(Object aObject)
+	{
+		var prefabType = PrefabUtility.GetPrefabType(aObject);
+		switch (prefabType)
+		{
+			case PrefabType.Prefab:
+			case PrefabType.ModelPrefab:
+				return true;
+			default:
+				return false;
+		}
+	}
+}
