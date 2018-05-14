10eb01c57ccc9b13415f6291e5a89ceb178e0e66
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-14 15:00:12 +0900

    EditorUtilityをEditorフォルダから出して、コンポーネントのOnValidateから参照できるように変更

--- MassShift/Assets/Scripts/Saito/EditorUtility.cs
+++ MassShift/Assets/Scripts/Saito/EditorUtility.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class EditorUtility {
 
-	public static bool IsPrefab(Object aObject)
-	{
+	public static bool IsPrefab(Object aObject) {
+
+#if UNITY_EDITOR
 		var prefabType = PrefabUtility.GetPrefabType(aObject);
 		switch (prefabType)
 		{
@@ -16,5 +20,8 @@ public class EditorUtility {
 			default:
 				return false;
 		}
+#endif
+		return false;
+
 	}
 }
