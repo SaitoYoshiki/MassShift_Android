fc5a86deca2c0c9ecb8c784f7d43cc8503805ae1
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-16 14:02:28 +0900

    めり込んでいた時のすり抜けをテスト

--- MassShift/Assets/Scripts/Saito/Test/OnewayFloorTest.cs
+++ MassShift/Assets/Scripts/Saito/Test/OnewayFloorTest.cs
@@ -14,15 +14,6 @@ public class OnewayFloorTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.U)) {
-			Debug.Log("Up        :" + mOnewayFloor.IsThrough(Vector3.up));
-			Debug.Log("Down      :" + mOnewayFloor.IsThrough(Vector3.down));
-			Debug.Log("Left      :" + mOnewayFloor.IsThrough(Vector3.left));
-			Debug.Log("Right     :" + mOnewayFloor.IsThrough(Vector3.right));
-			Debug.Log("UpRight   :" + mOnewayFloor.IsThrough(Vector3.up + Vector3.right));
-			Debug.Log("UpLeft    :" + mOnewayFloor.IsThrough(Vector3.up + Vector3.left));
-			Debug.Log("DownRight :" + mOnewayFloor.IsThrough(Vector3.down + Vector3.right));
-			Debug.Log("DownLeft  :" + mOnewayFloor.IsThrough(Vector3.down + Vector3.left));
-		}
+		Debug.Log(mOnewayFloor.IsThrough(Vector3.down, gameObject));
 	}
 }
