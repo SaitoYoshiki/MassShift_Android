af4e566425fedeb61163135d077080b74082ec29
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-19 12:30:54 +0900

    水の大きさを可変にする処理を実装

--- MassShift/Assets/Scripts/Saito/Water.cs
+++ MassShift/Assets/Scripts/Saito/Water.cs
@@ -15,28 +15,32 @@ public class Water : MonoBehaviour {
 	}
 
 	[SerializeField, Tooltip("横の大きさ")]
-	float mWidth;
+	int mWidth;
 
 	[SerializeField, Tooltip("縦の大きさ")]
-	float mHeight;
+	int mHeight;
 
-	[SerializeField, EditOnPrefab]
+	[SerializeField]
 	GameObject mModel;
 
-	[SerializeField, EditOnPrefab]
-	GameObject mCollider;
+	[SerializeField]
+	GameObject mWaterArea;
 
 #if UNITY_EDITOR
 
-	private void OnValidate() {
-
-		if (EditorUtility.IsPrefab(gameObject)) return;
+	[ContextMenu("Resize")]
+	void Resize() {
 
 		mModel.transform.localScale = new Vector3(mWidth, mHeight, 1.0f);
-		mModel.transform.localPosition = new Vector3(mWidth / 2.0f, -mHeight / 2.0f, 0.0f);
+		mModel.transform.localPosition = new Vector3(mWidth / 2.0f, mHeight / 2.0f, 0.0f);
 
-		mCollider.transform.localScale = new Vector3(mWidth, mHeight, 1.0f);
-		mCollider.transform.localPosition = new Vector3(mWidth / 2.0f, -mHeight / 2.0f, 0.0f);
+		mWaterArea.transform.localScale = new Vector3(mWidth, mHeight - 0.5f, 1.0f);
+		mWaterArea.transform.localPosition = new Vector3(mWidth / 2.0f, mHeight / 2.0f - 0.25f, 0.0f);
+	}
+
+	private void OnValidate() {
+		if (this == null) return;
+		if (EditorUtility.IsPrefab(gameObject)) return;
 	}
 
 #endif
