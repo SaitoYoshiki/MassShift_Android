46be4b11b6dccd42386d59b0d2fd9811f8454719
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-20 13:26:28 +0900

    ShareWeightBoxの処理を実装

---
+++ MassShift/Assets/Scripts/Saito/Test/ShareWeightBoxTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareWeightBoxTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ShareWeightBox s = GetComponent<ShareWeightBox>();
+		if(Input.GetKeyDown(KeyCode.A)) {
+			foreach(var ts in s.GetShareAllListExceptOwn()) {
+				Debug.Log(ts, this);
+			}
+		}
+	}
+}
