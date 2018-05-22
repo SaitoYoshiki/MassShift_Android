dfd780a723cd33e48ab7250f9c128f2d79de132f
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-22 23:36:31 +0900

    ステージのシーン名などを聞ける静的クラスを作成

---
+++ MassShift/Assets/Scripts/Saito/Area.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class Area {
+
+	List<int> mStageNum = new List<int>() {
+		3,	//チュートリアル
+		5,	//エリア１
+		5,	//エリア２
+		5	//エリア３
+	};
+
+	const string cStageSceneName = "Stage{0}-{1}";
+	const string cTutorialSceneName = "Tutorial-{0}";
+
+	static Area sInstance = null;
+	
+	//シングルトン
+	//
+	static Area GetInstance() {
+		if(sInstance == null) {
+			sInstance = new Area();
+		}
+		return sInstance;
+	}
+
+
+	//現在のシーン名を返す
+	//
+	static string GetSceneName() {
+		return UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+	}
+
+	//ステージのシーン名を返す
+	//
+	public static string GetStageSceneName(int aAreaIndex, int aStageIndex) {
+		if(aAreaIndex == 0) {
+			return string.Format(cTutorialSceneName, aStageIndex);
+		}
+		if(1 <= aAreaIndex && aAreaIndex < GetAreaNum()) {
+			return string.Format(cStageSceneName, aAreaIndex, aStageIndex);
+		}
+		return "";
+	}
+
+	//現在のエリア番号を返す（ステージ中なら、０・１・２・３．それ以外は-1）
+	//
+	public static int GetAreaIndex() {
+		string lSceneName = GetSceneName();
+
+		Regex r = new Regex(string.Format(cTutorialSceneName, @"(\d+?)"));
+
+		if (r.IsMatch(lSceneName)) {
+			return 0;
+		}
+
+		r = new Regex(string.Format(cStageSceneName, @"(\d+?)", @"(\d+?)"));
+
+		if(r.IsMatch(lSceneName)) {
+			var m = r.Match(lSceneName);
+			int lRes = 0;
+			int.TryParse(m.Groups[1].Value, out lRes);
+			return lRes;
+		}
+
+		return -1;
+	}
+
+	//現在のステージ番号を返す（ステージ中なら、０・１・２・３…。それ以外は-1）
+	//
+	public static int GetStageIndex() {
+		string lSceneName = GetSceneName();
+
+		Regex r = new Regex(string.Format(cTutorialSceneName, @"(\d+?)"));
+
+		if (r.IsMatch(lSceneName)) {
+			var m = r.Match(lSceneName);
+			int lRes = 0;
+			int.TryParse(m.Groups[1].Value, out lRes);
+			return lRes;
+		}
+
+		r = new Regex(string.Format(cStageSceneName, @"(\d+?)", @"(\d+?)"));
+
+		if (r.IsMatch(lSceneName)) {
+			var m = r.Match(lSceneName);
+			int lRes = 0;
+			int.TryParse(m.Groups[2].Value, out lRes);
+			return lRes;
+		}
+
+		return -1;
+	}
+
+
+	//エリア数を返す
+	//
+	public static int GetAreaNum() {
+		return GetInstance().mStageNum.Count;
+	}
+
+	//ステージ数を返す（エリア番号は、０・１・２・３）
+	//
+	public static int GetStageNum(int aAreaIndex) {
+		if (GetAreaNum() <= aAreaIndex) return -1;
+		if (aAreaIndex < 0) return -1;
+		return GetInstance().mStageNum[aAreaIndex];
+	}
+
+
+	//次のステージのシーン名を返す
+	//
+	public static string GetNextStageSceneName(int aAreaIndex, int aStageIndex) {
+		if (!ExistNextStage(aAreaIndex, aStageIndex)) return "";
+		if(!ExistNextStageSameArea(aAreaIndex, aStageIndex)) {
+			aAreaIndex += 1;
+			aStageIndex = 0;
+		}
+		else {
+			aStageIndex += 1;
+		}
+		return GetStageSceneName(aAreaIndex, aStageIndex);
+	}
+
+
+	//ゲーム中に、次のステージが存在しない
+	//
+	public static bool ExistNextStage(int aAreaIndex, int aStageIndex) {
+
+		int lStageListIndex = GetStageListIndex(aStageIndex);
+
+		//最終ステージだけ、次のステージが存在しない
+		if(GetAreaNum() - 1 == aAreaIndex) {
+			if (GetStageNum(aAreaIndex) - 1 == lStageListIndex) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	//同じエリアに、次のステージが存在しない
+	//
+	public static bool ExistNextStageSameArea(int aAreaIndex, int aStageIndex) {
+		//エリア3の場合、aAreaIndex→3
+		//GetAreaNum()→4となる
+		if (GetAreaNum() - 1 < aAreaIndex) return false;
+		if (aAreaIndex < 0) return false;
+
+		int lStageListIndex = GetStageListIndex(aStageIndex);
+		if (GetStageNum(aAreaIndex) - 1 <= lStageListIndex) return false;
+		if (lStageListIndex < 0) return false;
+
+		return true;
+	}
+
+
+	//ステージのインデックスは１・２・３で、配列を参照するためにずらす
+	//
+	static int GetStageListIndex(int aStageIndex) {
+		return aStageIndex - 1;
+	}
+}
---
+++ MassShift/Assets/Scripts/Saito/Test/AreaTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("AreaIndex:" + Area.GetAreaIndex());
+		Debug.Log("StageIndex:" + Area.GetStageIndex());
+
+		Debug.Log("AreaNum:" + Area.GetAreaNum());
+		Debug.Log("Area0-StageNum:" + Area.GetStageNum(0));
+		Debug.Log("Area1-StageNum:" + Area.GetStageNum(1));
+		Debug.Log("Area2-StageNum:" + Area.GetStageNum(2));
+		Debug.Log("Area3-StageNum:" + Area.GetStageNum(3));
+
+		Debug.Log("Stage1-1のシーン名:" + Area.GetStageSceneName(1, 1));
+		Debug.Log("Stage0-3のシーン名:" + Area.GetStageSceneName(0, 3));
+		Debug.Log("Stage3-0のシーン名:" + Area.GetStageSceneName(3, 0));
+
+		Debug.Log("Stage0-1の次のシーン:" + Area.GetNextStageSceneName(0, 1));
+		Debug.Log("Stage0-3の次のシーン:" + Area.GetNextStageSceneName(0, 3));
+		Debug.Log("Stage1-1の次のシーン:" + Area.GetNextStageSceneName(1, 1));
+		Debug.Log("Stage1-5の次のシーン:" + Area.GetNextStageSceneName(1, 5));
+		Debug.Log("Stage2-5の次のシーン:" + Area.GetNextStageSceneName(2, 5));
+		Debug.Log("Stage3-5の次のシーン:" + Area.GetNextStageSceneName(3, 5));
+
+		Debug.Log("Stage0-1の次のシーンがあるか:" + Area.ExistNextStage(0, 1));
+		Debug.Log("Stage2-5の次のシーンがあるか:" + Area.ExistNextStage(2, 5));
+		Debug.Log("Stage1-3の次のシーンがあるか:" + Area.ExistNextStage(1, 3));
+		Debug.Log("Stage3-5の次のシーンがあるか:" + Area.ExistNextStage(3, 5));
+
+		Debug.Log("Stage0-1の次のシーンが同じエリアにあるか:" + Area.ExistNextStageSameArea(0, 1));
+		Debug.Log("Stage0-3の次のシーンが同じエリアにあるか:" + Area.ExistNextStageSameArea(0, 3));
+		Debug.Log("Stage1-1の次のシーンが同じエリアにあるか:" + Area.ExistNextStageSameArea(1, 1));
+		Debug.Log("Stage1-5の次のシーンが同じエリアにあるか:" + Area.ExistNextStageSameArea(1, 5));
+		Debug.Log("Stage2-3の次のシーンが同じエリアにあるか:" + Area.ExistNextStageSameArea(2, 3));
+		Debug.Log("Stage3-5の次のシーンが同じエリアにあるか:" + Area.ExistNextStageSameArea(3, 5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
