dac89b0476b611f5b09c22df758261f092e04f3a
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-24 05:10:28 +0900

    落下の衝撃のイベントの仕組みを作成

---
+++ MassShift/Assets/Scripts/Saito/LandImpact.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Landing)), RequireComponent(typeof(WeightManager))]
+public class LandImpact : MonoBehaviour {
+
+	public delegate void OnLandEvent(WeightManager.Weight aWeight);
+
+	public event OnLandEvent OnLand;
+
+	[SerializeField, Tooltip("この距離以上を落下すると、落下演出が起きる"), EditOnPrefab]
+	float mImpactDistance = 1.0f;
+
+
+	Vector3 mBeforePosition;	//前回の位置を保存
+	Vector3 mHighestPosition;	//落下時の、最高地点を保存
+
+	bool mBeforeLanding = false;    //前のフレームで設置していたか
+
+	//コンポーネントのキャッシュ
+	Landing mLanding;
+	WeightManager mWeightManager;
+
+
+	// Use this for initialization
+	void Start () {
+		mLanding = GetComponent<Landing>();
+		mWeightManager = GetComponent<WeightManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.deltaTime == 0.0f) {
+			return;
+		}
+
+		//最高地点の更新
+		//
+
+		//上向きに進んでいたら
+		if (transform.position.y >= mBeforePosition.y) {
+			mHighestPosition = transform.position;	//最高地点を更新
+		}
+		mBeforePosition = transform.position;
+
+
+		//接地判定
+		//
+		bool lLanding = mLanding.IsLanding;
+
+		//このフレームに接地し始めていて
+		if (mBeforeLanding == false && lLanding == true) {
+
+			//一定距離以上落ちていたら
+			if (mHighestPosition.y - transform.position.y >= mImpactDistance) {
+				OnLand(mWeightManager.WeightLv);	//インパクトのイベントを呼び出す
+			}
+		}
+		mBeforeLanding = lLanding;
+	}
+
+}
---
+++ MassShift/Assets/Scripts/Saito/LandShake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LandImpact))]
+public class LandShake : MonoBehaviour {
+
+	[SerializeField, Tooltip("揺れる時間"), EditOnPrefab]
+	float mShakeTime = 0.2f;
+
+	// Use this for initialization
+	void Awake () {
+		GetComponent<LandImpact>().OnLand += OnLand;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnLand(WeightManager.Weight aWeight) {
+		if(aWeight == WeightManager.Weight.heavy) {
+			ShakeCamera.ShakeAll(mShakeTime);
+		}
+		Debug.Log("OnLand!!");
+	}
+}
