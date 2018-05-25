c7ceff83a74d564556e96f3ba4d3f3557ed2f263
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-26 01:26:52 +0900

    重さを移せないときに再生するアニメーションを、ボックスに追加

--- MassShift/Assets/Scripts/Saito/MassShiftFailedAnimation.cs
+++ MassShift/Assets/Scripts/Saito/MassShiftFailedAnimation.cs
@@ -5,71 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(MassShiftFailed))]
 public class MassShiftFailedAnimation : MonoBehaviour {
 
-	[SerializeField, EditOnPrefab]
-	GameObject mFlyingParticlePrefab;
+	[SerializeField]
+	Animator mAnimator;
 
-	[SerializeField, EditOnPrefab]
-	GameObject mLightParticlePrefab;
-
-	[SerializeField, EditOnPrefab]
-	GameObject mHeavyParticlePrefab;
-
-	[SerializeField, EditOnPrefab]
-	GameObject mParticleParent;
-
-	GameObject mFlyingParticle;
-
-	GameObject mLightParticle;
-
-	GameObject mHeavyParticle;
+	[SerializeField]
+	string mAnimationName;
 
 	// Use this for initialization
 	void Awake () {
-
-		mFlyingParticle = Instantiate(mFlyingParticlePrefab, mParticleParent.transform);
-		mLightParticle = Instantiate(mLightParticlePrefab, mParticleParent.transform);
-		mHeavyParticle = Instantiate(mHeavyParticlePrefab, mParticleParent.transform);
-
-		GetComponent<WeightEffect>().OnWeightChange += ChangeParticle;
+		GetComponent<MassShiftFailed>().OnMassShiftFailed += PlayAnimation;
 	}
 	
-	void ChangeParticle(WeightManager.Weight aWeight) {
-
-		if (aWeight == WeightManager.Weight.flying) {
-			PlayParticle(mFlyingParticle);
-			StopParticle(mHeavyParticle);
-			StopParticle(mLightParticle);
-		}
-
-		if (aWeight == WeightManager.Weight.light) {
-			PlayParticle(mLightParticle);
-			StopParticle(mHeavyParticle);
-			StopParticle(mFlyingParticle);
-		}
-
-		if (aWeight == WeightManager.Weight.heavy) {
-			PlayParticle(mHeavyParticle);
-			StopParticle(mLightParticle);
-			StopParticle(mFlyingParticle);
-		}
-	}
-
-	void PlayParticle(GameObject aWeightParticle) {
-
-		foreach (var p in aWeightParticle.GetComponentsInChildren<ParticleSystem>()) {
-			p.Play();
-		}
-		foreach (var p in aWeightParticle.GetComponentsInChildren<MeshRenderer>()) {
-			p.enabled = true;
-		}
-	}
-	void StopParticle(GameObject aWeightParticle) {
-
-		foreach (var p in aWeightParticle.GetComponentsInChildren<ParticleSystem>()) {
-			p.Stop();
-		}
-		foreach (var p in aWeightParticle.GetComponentsInChildren<MeshRenderer>()) {
-			p.enabled = false;
-		}
+	void PlayAnimation() {
+		mAnimator.Play(mAnimationName);
 	}
 }
