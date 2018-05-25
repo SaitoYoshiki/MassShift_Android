fba3cb7b458b1ba2fc4af63812527942ecd7f080
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-26 00:13:41 +0900

    重さを移すのに失敗したときのイベントのコンポーネントを追加

---
+++ MassShift/Assets/Scripts/Saito/MassShiftFailedAnimation.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MassShiftFailed))]
+public class MassShiftFailedAnimation : MonoBehaviour {
+
+	[SerializeField, EditOnPrefab]
+	GameObject mFlyingParticlePrefab;
+
+	[SerializeField, EditOnPrefab]
+	GameObject mLightParticlePrefab;
+
+	[SerializeField, EditOnPrefab]
+	GameObject mHeavyParticlePrefab;
+
+	[SerializeField, EditOnPrefab]
+	GameObject mParticleParent;
+
+	GameObject mFlyingParticle;
+
+	GameObject mLightParticle;
+
+	GameObject mHeavyParticle;
+
+	// Use this for initialization
+	void Awake () {
+
+		mFlyingParticle = Instantiate(mFlyingParticlePrefab, mParticleParent.transform);
+		mLightParticle = Instantiate(mLightParticlePrefab, mParticleParent.transform);
+		mHeavyParticle = Instantiate(mHeavyParticlePrefab, mParticleParent.transform);
+
+		GetComponent<WeightEffect>().OnWeightChange += ChangeParticle;
+	}
+	
+	void ChangeParticle(WeightManager.Weight aWeight) {
+
+		if (aWeight == WeightManager.Weight.flying) {
+			PlayParticle(mFlyingParticle);
+			StopParticle(mHeavyParticle);
+			StopParticle(mLightParticle);
+		}
+
+		if (aWeight == WeightManager.Weight.light) {
+			PlayParticle(mLightParticle);
+			StopParticle(mHeavyParticle);
+			StopParticle(mFlyingParticle);
+		}
+
+		if (aWeight == WeightManager.Weight.heavy) {
+			PlayParticle(mHeavyParticle);
+			StopParticle(mLightParticle);
+			StopParticle(mFlyingParticle);
+		}
+	}
+
+	void PlayParticle(GameObject aWeightParticle) {
+
+		foreach (var p in aWeightParticle.GetComponentsInChildren<ParticleSystem>()) {
+			p.Play();
+		}
+		foreach (var p in aWeightParticle.GetComponentsInChildren<MeshRenderer>()) {
+			p.enabled = true;
+		}
+	}
+	void StopParticle(GameObject aWeightParticle) {
+
+		foreach (var p in aWeightParticle.GetComponentsInChildren<ParticleSystem>()) {
+			p.Stop();
+		}
+		foreach (var p in aWeightParticle.GetComponentsInChildren<MeshRenderer>()) {
+			p.enabled = false;
+		}
+	}
+}
