44440e3daa12212d0906bef7a193af919e48216b
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-26 21:01:52 +0900

    水面に落下したとき、SEがなるように実装

--- MassShift/Assets/Scripts/Saito/LandImpactSound.cs
+++ MassShift/Assets/Scripts/Saito/LandImpactSound.cs
@@ -11,6 +11,12 @@ public class LandImpactSound : MonoBehaviour {
 	[SerializeField, Tooltip("重さ1で落下したときのSE"), EditOnPrefab]
 	GameObject mLandImpactLightSE;
 
+	[SerializeField, Tooltip("重さ2で水に落下したときのSE"), EditOnPrefab]
+	GameObject mLandImpactWaterHeavySE;
+
+	[SerializeField, Tooltip("重さ1で水に落下したときのSE"), EditOnPrefab]
+	GameObject mLandImpactWaterLightSE;
+
 	// Use this for initialization
 	void Awake() {
 		GetComponent<LandImpact>().OnLand += OnLand;
@@ -22,11 +28,22 @@ public class LandImpactSound : MonoBehaviour {
 	}
 
 	void OnLand(WeightManager.Weight aWeight, bool aIsWater) {
-		if (aWeight == WeightManager.Weight.heavy) {
-			SoundManager.SPlay(mLandImpactHeavySE);
+
+		if(aIsWater) {
+			if (aWeight == WeightManager.Weight.heavy) {
+				SoundManager.SPlay(mLandImpactWaterHeavySE);
+			}
+			if (aWeight == WeightManager.Weight.light) {
+				SoundManager.SPlay(mLandImpactWaterLightSE);
+			}
 		}
-		if (aWeight == WeightManager.Weight.light) {
-			SoundManager.SPlay(mLandImpactLightSE);
+		else {
+			if (aWeight == WeightManager.Weight.heavy) {
+				SoundManager.SPlay(mLandImpactHeavySE);
+			}
+			if (aWeight == WeightManager.Weight.light) {
+				SoundManager.SPlay(mLandImpactLightSE);
+			}
 		}
 	}
 }
