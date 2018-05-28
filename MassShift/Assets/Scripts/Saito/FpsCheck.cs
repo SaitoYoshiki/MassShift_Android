99deb83404e194200752c3f7a1970acdc3da7332
SaitoYoshiki <saitosaito888888@gmail.com>
2018-05-28 23:01:42 +0900

    fpsを表示する機能を追加

---
+++ MassShift/Assets/Scripts/Saito/FpsCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCheck : MonoBehaviour {
+
+	[SerializeField]
+	TextMesh mText;
+
+	[SerializeField]
+	float mCountFpsMinTime = 0.5f;
+
+	int frameCount;
+	float prevTime;
+
+	// Use this for initialization
+	void Start () {
+		frameCount = 0;
+		prevTime = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		frameCount++;
+
+		float lTakeTime = Time.realtimeSinceStartup - prevTime;
+		if(lTakeTime >= mCountFpsMinTime) {
+			mText.text = string.Format("fps:{0}", frameCount /  lTakeTime);
+			frameCount = 0;
+			prevTime = Time.realtimeSinceStartup;
+		}
+	}
+}
